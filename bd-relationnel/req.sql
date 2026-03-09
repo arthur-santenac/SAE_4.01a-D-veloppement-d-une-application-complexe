4cc9fc0f2f3a332277181876eb5f3384f6123a1d
arthur-santenac <arthur.santenac@etu.univ-orleans.fr>
2026-03-09 17:42:45 +0100

    ajout vol récursif peut importe correspondance

--- bd-relationnel/req.sql
+++ bd-relationnel/req.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT vi_arr.nomVille
 FROM VOL v
 JOIN AEROPORT a_dep ON v.idAeroportDep = a_dep.idAeroport
 JOIN VILLE vi_dep ON a_dep.idVille = vi_dep.idVille
-JOIN AEROPORT a_arr ON v.idAeroportArr = a_arr.idAeroport
+JOIN AEROPORT a_arr ON v.idAeroportArr = a_arr.idAeroport 
 JOIN VILLE vi_arr ON a_arr.idVille = vi_arr.idVille
 WHERE vi_dep.nomVille = 'Paris';
 
@@ -30,4 +30,21 @@ JOIN AEROPORT a_arr3 ON v3.idAeroportArr = a_arr3.idAeroport
 JOIN VILLE vi_arr3 ON a_arr3.idVille = vi_arr3.idVille
 WHERE vi_dep1.nomVille = 'Paris'
   AND v1.dateHeureArr <= v2.dateHeureDep
-  AND v2.dateHeureArr <= v3.dateHeureDep;Z
+  AND v2.dateHeureArr <= v3.dateHeureDep;
+
+WITH VolsAccessibles (idAeroportArr, dateHeureArr) AS (
+  SELECT v.idAeroportArr, v.dateHeureArr
+  FROM VOL v
+  JOIN AEROPORT a_dep ON v.idAeroportDep = a_dep.idAeroport
+  JOIN VILLE vi_dep ON a_dep.idVille = vi_dep.idVille
+  WHERE vi_dep.nomVille = 'Paris'
+  UNION ALL
+  SELECT v_suiv.idAeroportArr, v_suiv.dateHeureArr
+  FROM VolsAccessibles va
+  JOIN VOL v_suiv ON va.idAeroportArr = v_suiv.idAeroportDep
+  WHERE va.dateHeureArr <= v_suiv.dateHeureDep
+)
+SELECT DISTINCT vi_arr.nomVille
+FROM VolsAccessibles va
+JOIN AEROPORT a_arr ON va.idAeroportArr = a_arr.idAeroport
+JOIN VILLE vi_arr ON a_arr.idVille = vi_arr.idVille;
